3e6eb3b1a44ed3fb7a66433bb7114928ecc2238f
Mohamed <mohamed.terbaoui@umontreal.ca>
2025-03-20 17:13:13 -0400

    added functionality to operations

--- script.js
+++ script.js
@@ -1,57 +1,135 @@
-const calculatorBtns = document.querySelector(".calculator-btns");
-const memory = document.querySelector("#memory");
-const input = document.querySelector("#input");
-const buttons = [
-  "C",
-  "exp",
-  "mod",
-  "n!",
-  7,
-  8,
-  9,
-  "×",
-  4,
-  5,
-  6,
-  "-",
-  1,
-  2,
-  3,
-  "+",
-  "+/-",
-  0,
-  ".",
-  "=",
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "+/-", 0, "."];
+const operations = [
+  { id: "plus", text: "+", "+": (a, b) => Number(a) + Number(b) },
+  { id: "minus", text: "-", "-": (a, b) => a - b },
+  { id: "times", text: "x", x: (a, b) => a * b },
+  { id: "divide", text: "/", "/": (a, b) => a / b },
+  { id: "exponential", text: "^", "^": (a, b) => a ** b },
+  { id: "modulo", text: "%", "%": (a, b) => a % b },
 ];
 
-const numberLimit = 16;
+const options = [
+  { id: "clear", text: "C" },
+  { id: "equal", text: "=" },
+];
 
-const onBtnClick = function (number) {
-  if (input.textContent.length < numberLimit) {
-    input.textContent += number;
+const input = document.querySelector("#input");
+const memory = document.querySelector("#memory");
+
+const btnNumbers = document.querySelector(".numbers");
+const btnOperations = document.querySelector(".operations");
+
+const maxNumbers = 16;
+
+const evaluate = function (firstOperand, secondOperand, operation) {
+  if (memory.textContent && input.textContent) {
+    const op = operations.find((op) => op.text === operation);
+    const result = op[operation](firstOperand, secondOperand);
+    return result;
   }
 };
 
-const displayButtons = function (buttons) {
-  for (let i = 0; i < buttons.length; i++) {
-    const btn = document.createElement("button");
-    btn.textContent = buttons[i];
-    btn.classList.add("btn");
-    btn.id = buttons[i];
-    calculatorBtns.append(btn);
-    btn.addEventListener("click", () => onBtnClick(btn.textContent));
-  }
+const drawButtons = function () {
+  numbers.forEach((element) => {
+    const btn = document.createElement("div");
+    btn.classList.add("nbr-btn");
+    btn.textContent = element;
+    btnNumbers.append(btn);
+
+    if (element !== "+/-" && element !== ".") {
+      btn.addEventListener("click", () => {
+        if (input.textContent.length <= maxNumbers) {
+          input.textContent += btn.textContent;
+        }
+      });
+    }
+
+    if (element === ".") {
+      btn.addEventListener("click", () => {
+        if (!input.textContent.includes(".") && input.textContent) {
+          input.textContent += ".";
+        }
+      });
+    } else if (element === "+/-") {
+      btn.addEventListener("click", () => {
+        if (input.textContent) {
+          if (input.textContent.includes("-")) {
+            input.textContent = input.textContent.slice(1);
+          } else {
+            input.textContent = "-" + input.textContent;
+          }
+        }
+      });
+    }
+  });
+
+  operations.forEach((element) => {
+    const btn = document.createElement("div");
+    btn.classList.add("op-btn");
+    btn.textContent = element.text;
+    btn.id = element.id;
+    btnOperations.append(btn);
+
+    btn.addEventListener("click", () => {
+      if (!memory.textContent) {
+        if (input.textContent) {
+          input.textContent += " " + btn.textContent;
+          memory.textContent = input.textContent;
+          input.textContent = "";
+        }
+      } else {
+        if (input.textContent) {
+          const firstPart = memory.textContent.split(" ");
+          const firstOperand = firstPart[0];
+          const operation = firstPart[1];
+          const secondOperand = input.textContent;
+
+          const result = evaluate(firstOperand, secondOperand, operation);
+
+          memory.textContent = result + " " + btn.textContent;
+          input.textContent = "";
+        } else {
+          const temp = memory.textContent.split(" ");
+
+          memory.textContent = temp[0] + " " + btn.textContent;
+        }
+      }
+    });
+  });
+
+  const equal = document.createElement("div");
+  const dot = document.createElement("div");
+  const clear = document.createElement("div");
+
+  equal.textContent = "=";
+  clear.textContent = "C";
+
+  equal.classList.add("op-btn");
+  clear.classList.add("op-btn");
+
+  equal.id = "equal";
+  clear.id = "clear";
+
+  btnOperations.append(clear);
+  btnOperations.append(equal);
+
+  equal.addEventListener("click", () => {
+    if (memory.textContent && input.textContent) {
+      const firstPart = memory.textContent.split(" ");
+      const firstOperand = firstPart[0];
+      const operation = firstPart[1];
+      const secondOperand = input.textContent;
+      const result = evaluate(firstOperand, secondOperand, operation);
+
+      input.textContent = result;
+      memory.textContent = "";
+    }
+  });
+
+  clear.addEventListener("click", () => {
+    memory.textContent = "";
+    input.textContent = "";
+  });
 };
 
-const addEventToBtn = function (btn, func) {
-  const element = document.querySelector(`#${btn}`);
-  element.addEventListener("click", func);
-};
-
-const clearInput = function () {
-  input.textContent = "";
-  input.textContent = "";
-};
-
-displayButtons(buttons);
-addEventToBtn("C", clearInput);
+drawButtons();
